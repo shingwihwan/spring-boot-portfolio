30d18cf43bb9661971a8bc4f16fd695cc574938b
shin <zerll9100@gmail.com>
2021-08-04 16:20:26 +0900

    전체 조회 화면 만들기

--- src/main/java/com/zamar/www/springboot/service/PostsService.java
+++ src/main/java/com/zamar/www/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.zamar.www.springboot.service;
 
 import com.zamar.www.springboot.domain.posts.Posts;
 import com.zamar.www.springboot.domain.posts.PostsRepository;
+import com.zamar.www.springboot.web.dto.PostsListResponsDto;
 import com.zamar.www.springboot.web.dto.PostsResponseDto;
 import com.zamar.www.springboot.web.dto.PostsSaveRequestDto;
 import com.zamar.www.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,8 +32,14 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponsDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponsDto::new).collect(Collectors.toList());
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
         return new PostsResponseDto(entity);
     }
+
 }
--- src/main/java/com/zamar/www/springboot/web/IndexController.java
+++ src/main/java/com/zamar/www/springboot/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.zamar.www.springboot.web;
 
+import com.zamar.www.springboot.domain.posts.PostsRepository;
+import com.zamar.www.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
