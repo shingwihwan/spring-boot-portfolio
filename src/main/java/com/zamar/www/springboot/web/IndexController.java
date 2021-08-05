5bec39395f357343178edb284a700fc3c6cca5b8
shin <zerll9100@gmail.com>
2021-08-05 22:44:42 +0900

    구글 로그인 연동하기 - User, Role, UserRepository, build.gradle(oauth2-clinet)추가, SecurityConfig, CustomOAuth2UserService, OAuthAttributes + 어노테이션 추가(LoginUser, LoginUserArgumentResolver, WebConfig)

--- src/main/java/com/zamar/www/springboot/web/IndexController.java
+++ src/main/java/com/zamar/www/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.zamar.www.springboot.web;
 
+import com.zamar.www.springboot.config.auth.LoginUser;
+import com.zamar.www.springboot.config.auth.dto.SessionUser;
 import com.zamar.www.springboot.domain.posts.PostsRepository;
 import com.zamar.www.springboot.service.PostsService;
 import com.zamar.www.springboot.web.dto.PostsResponseDto;
@@ -9,15 +11,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
