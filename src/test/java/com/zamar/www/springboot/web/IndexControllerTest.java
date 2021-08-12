d78b4ab089af4958dcee11c44a92a29455ac5169
shin <zerll9100@gmail.com>
2021-08-12 19:27:16 +0900

    ****************front 꾸미기 시작***************** 1차 홈화면 꾸민것

--- src/test/java/com/zamar/www/springboot/web/IndexControllerTest.java
+++ src/test/java/com/zamar/www/springboot/web/IndexControllerTest.java
@@ -22,7 +22,7 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("로그인 하시면 글을 쓸 수 있습니다.");
 
     }
 }
